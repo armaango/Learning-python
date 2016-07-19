5d52837a69e027a6bd34c3ec9a01c53c5fbd8888
armaango <armaan.goyal@gmail.com>
2016-07-19 00:18:53 -0400

    added new files

--- Python Fundamentals 2.7/Hackerrank 30 days of code Dicts and Maps.py
+++ Python Fundamentals 2.7/Hackerrank 30 days of code Dicts and Maps.py
@@ -1,14 +1,17 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-cases = int(raw_input())
+
+cases = long(raw_input())
 phoneBook = dict()
 for i in range(0,cases):
     x,y = raw_input().split()
     phoneBook[x]=y
-t = raw_input()
-while(t != ""):
-    if t in phoneBook.keys():
-        print(t+"="+phoneBook[t])
-    else:
-        print "Not found"
-    t = raw_input()
+while True:
+    try:
+        inp = raw_input()
+        if inp in phoneBook:
+            print(inp+"="+phoneBook[inp])
+        else:
+            print "Not found"
+    except:
+        break
